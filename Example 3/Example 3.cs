3fb1d78bac85751625184b0f1b8636f3208b44e8
Mystique <zorinatrader@gmail.com>
2023-01-28 00:51:25 +0300

    algemra random1

--- Example 3/Example 3.cs
+++ Example 3/Example 3.cs
@@ -1,1 +1,6 @@
-﻿
+﻿double x = new Random().Next(1, 10); // от 1 до 9
+Console.WriteLine(x);
+double y = new Random().Next(1, 10); // от 1 до 9
+Console.WriteLine(y);
+double result = x/y;
+Console.WriteLine(result);
