0abe7f9715b607841a961184359533a7e2aa52a5
Mystique <zorinatrader@gmail.com>
2023-01-28 01:10:23 +0300

    приветствие любимчика

---
+++ приветствие любимчика/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("ВВедите имя пользователя");
+string username = Console.ReadLine();
+
+if (username =="Маша")
+{
+    Console.Write("Урa, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
