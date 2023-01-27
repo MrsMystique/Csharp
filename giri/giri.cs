4df59eda16edd449f6081b8e809f0860c34a8278
Mystique <zorinatrader@gmail.com>
2023-01-28 01:28:49 +0300

    giri

---
+++ giri/giri.cs
@@ -0,0 +1,14 @@
+﻿int a = 3;
+int b = 6;
+int d = 10;
+int c = 4;
+int e = 2;
+
+int max = a;
+if (a>max) max=a;
+if (b>max) max=b;
+if (d>max) max=d;
+if (c>max) max=c;
+if (e>max) max=e;
+Console.Write("max= ");
+Console.WriteLine(max);
