19b59d26b0eb3457554ecc256871fe10d62c2983
Mystique <zorinatrader@gmail.com>
2023-01-28 01:47:42 +0300

    triangle

---
+++ loop/loop.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10,4);
+//Console.Write("+");
+//Console.WriteLine("+");
+int xa = 10;
+int ya = 20;
+int xb = 10;
+int yb = 30;
+int xc = 80;
+int yc = 30;
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine("***");
+
+Console.SetCursorPosition(xb,yb);
+Console.WriteLine("***");
+
+Console.SetCursorPosition(xc,yc);
+Console.WriteLine("***");
